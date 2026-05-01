2fcb2f40f6fece6fbd88bcf8f18a36a2d98a2073
Aleksei Sheludchenkov <aleshkashell@gmail.com>
2026-05-01 21:33:45 +0300

    Replace hyprland repo

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 COPR_REPOS=(
-    sdegler/hyprland
+    # sdegler/hyprland
+    lionheartp/Hyprland
     erikreider/SwayNotificationCenter
     errornointernet/packages
     tofik/nwg-shell
