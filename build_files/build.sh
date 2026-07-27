2a3ae2b16cb63c0de14cf95871fbebb056750aff
Sheludchenkov Aleksei <aleshkashell@gmail.com>
2026-07-27 18:04:56 +0300

    Add guix folder

--- build_files/build.sh
+++ build_files/build.sh
@@ -162,6 +162,7 @@ done
 rm /etc/yum.repos.d/charm.repo
 
 mkdir /nix
+mkdir /gnu
 # If you want to use prebuilt packages, you should run before using guix package
 for i in /usr/share/guix/*.pub; do guix archive --authorize < "$i"; done
 
